175762232faf7caf857e9602d73e8e242aee6dbd
RonakSurana-2001 <100184606+RonakSurana-2001@users.noreply.github.com>
2022-02-24 09:19:17 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -6,4 +6,11 @@ int main()
   ll a,b;
   cin>>a>>b;
   cout<<"The sum of numbers is"<<a+b<<endl;
+  int arr[]={1,2,3,4};
+  int sum=0;
+  for(int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++)
+      {
+        sum+=arr[i];
+      }
+  cout<<sum<<endl;
 }
